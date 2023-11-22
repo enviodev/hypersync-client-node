9ff151f9649413637663c1f8f86a28742054996d
Ozgur Akkurt <oezgurmakkurt@gmail.com>
2023-11-22 04:24:05 +0300

    add simpler default api, update ci

--- src/lib.rs
+++ src/lib.rs
@@ -15,71 +15,106 @@ use types::{Block, Event, Log, Transaction};
 
 #[napi]
 pub struct SkarClient {
-  inner: skar_client::Client,
+    inner: skar_client::Client,
 }
 
 #[napi]
 impl SkarClient {
-  #[napi]
-  pub fn new(cfg: Config) -> napi::Result<SkarClient> {
-    let cfg = cfg
-      .try_convert()
-      .map_err(|e| napi::Error::from_reason(format!("{:?}", e)))?;
+    #[napi]
+    pub fn new(cfg: Config) -> napi::Result<SkarClient> {
+        let cfg = cfg
+            .try_convert()
+            .map_err(|e| napi::Error::from_reason(format!("{:?}", e)))?;
 
-    Ok(SkarClient {
-      inner: skar_client::Client::new(cfg),
-    })
-  }
-
-  #[napi]
-  pub async fn get_height(&self) -> napi::Result<i64> {
-    let height = self
-      .inner
-      .get_height()
-      .await
-      .map_err(|e| napi::Error::from_reason(format!("{:?}", e)))?;
-
-    Ok(height.try_into().unwrap())
-  }
-
-  #[napi]
-  pub async fn send_events_req(&self, query: Query) -> napi::Result<Events> {
-    self
-      .send_events_req_impl(query)
-      .await
-      .map_err(|e| napi::Error::from_reason(format!("{:?}", e)))
-  }
-
-  async fn send_events_req_impl(&self, query: Query) -> Result<Events> {
-    let mut query = query.try_convert().context("parse query")?;
-
-    if !query.field_selection.block.is_empty() {
-      for field in BLOCK_JOIN_FIELDS.iter() {
-        query.field_selection.block.insert(field.to_string());
-      }
+        Ok(SkarClient {
+            inner: skar_client::Client::new(cfg),
+        })
     }
 
-    if !query.field_selection.transaction.is_empty() {
-      for field in TX_JOIN_FIELDS.iter() {
-        query.field_selection.transaction.insert(field.to_string());
-      }
+    #[napi]
+    pub async fn get_height(&self) -> napi::Result<i64> {
+        let height = self
+            .inner
+            .get_height()
+            .await
+            .map_err(|e| napi::Error::from_reason(format!("{:?}", e)))?;
+
+        Ok(height.try_into().unwrap())
     }
 
-    if !query.field_selection.log.is_empty() {
-      for field in LOG_JOIN_FIELDS.iter() {
-        query.field_selection.log.insert(field.to_string());
-      }
+    #[napi]
+    pub async fn send_req(&self, query: Query) -> napi::Result<QueryResponse> {
+        self.send_req_impl(query)
+            .await
+            .map_err(|e| napi::Error::from_reason(format!("{:?}", e)))
     }
 
-    let res = self
-      .inner
-      .send::<skar_client::ArrowIpc>(&query)
-      .await
-      .context("execute query")?;
-    let res = convert_response_to_events(res).context("convert response to js format")?;
+    async fn send_req_impl(&self, query: Query) -> Result<QueryResponse> {
+        let query = query.try_convert().context("parse query")?;
 
-    Ok(res)
-  }
+        let res = self
+            .inner
+            .send::<skar_client::ArrowIpc>(&query)
+            .await
+            .context("execute query")?;
+        let res =
+            convert_response_to_query_response(res).context("convert response to js format")?;
+
+        Ok(res)
+    }
+
+    #[napi]
+    pub async fn send_events_req(&self, query: Query) -> napi::Result<Events> {
+        self.send_events_req_impl(query)
+            .await
+            .map_err(|e| napi::Error::from_reason(format!("{:?}", e)))
+    }
+
+    async fn send_events_req_impl(&self, query: Query) -> Result<Events> {
+        let mut query = query.try_convert().context("parse query")?;
+
+        if !query.field_selection.block.is_empty() {
+            for field in BLOCK_JOIN_FIELDS.iter() {
+                query.field_selection.block.insert(field.to_string());
+            }
+        }
+
+        if !query.field_selection.transaction.is_empty() {
+            for field in TX_JOIN_FIELDS.iter() {
+                query.field_selection.transaction.insert(field.to_string());
+            }
+        }
+
+        if !query.field_selection.log.is_empty() {
+            for field in LOG_JOIN_FIELDS.iter() {
+                query.field_selection.log.insert(field.to_string());
+            }
+        }
+
+        let res = self
+            .inner
+            .send::<skar_client::ArrowIpc>(&query)
+            .await
+            .context("execute query")?;
+        let res = convert_response_to_events(res).context("convert response to js format")?;
+
+        Ok(res)
+    }
+}
+
+#[napi(object)]
+pub struct QueryResponseData {
+    pub blocks: Vec<Block>,
+    pub transactions: Vec<Transaction>,
+    pub logs: Vec<Log>,
+}
+
+#[napi(object)]
+pub struct QueryResponse {
+    pub archive_height: Option<i64>,
+    pub next_block: i64,
+    pub total_execution_time: i64,
+    pub data: QueryResponseData,
 }
 
 const BLOCK_JOIN_FIELDS: &[&str] = &["number"];
@@ -88,37 +123,72 @@ const LOG_JOIN_FIELDS: &[&str] = &["log_index", "transaction_index", "block_numb
 
 #[napi(object)]
 pub struct Events {
-  pub archive_height: Option<i64>,
-  pub next_block: i64,
-  pub total_execution_time: i64,
-  pub events: Vec<Event>,
+    pub archive_height: Option<i64>,
+    pub next_block: i64,
+    pub total_execution_time: i64,
+    pub events: Vec<Event>,
 }
 
 fn convert_response_to_events(res: skar_client::QueryResponse) -> Result<Events> {
-  let blocks = Block::from_batches(&res.data.blocks).context("map blocks from arrow")?;
+    let blocks = Block::from_batches(&res.data.blocks).context("map blocks from arrow")?;
 
-  let txs =
-    Transaction::from_batches(&res.data.transactions).context("map transactions from arrow")?;
+    let txs =
+        Transaction::from_batches(&res.data.transactions).context("map transactions from arrow")?;
 
-  let logs = Log::from_batches(&res.data.logs).context("map logs from arrow")?;
+    let logs = Log::from_batches(&res.data.logs).context("map logs from arrow")?;
 
-  let mut events = Vec::with_capacity(logs.len());
+    let mut events = Vec::with_capacity(logs.len());
 
-  for (_, log) in logs.into_iter() {
-    let transaction = txs.get(&(log.block_number, log.transaction_index)).cloned();
-    let block = blocks.get(&log.block_number).cloned();
+    for (_, log) in logs.into_iter() {
+        let transaction = txs.get(&(log.block_number, log.transaction_index)).cloned();
+        let block = blocks.get(&log.block_number).cloned();
 
-    events.push(Event {
-      log,
-      block,
-      transaction,
+        events.push(Event {
+            log,
+            block,
+            transaction,
+        })
+    }
+
+    Ok(Events {
+        archive_height: res.archive_height.map(|h| h.try_into().unwrap()),
+        next_block: res.next_block.try_into().unwrap(),
+        total_execution_time: res.total_execution_time.try_into().unwrap(),
+        events,
     })
-  }
+}
 
-  Ok(Events {
-    archive_height: res.archive_height.map(|h| h.try_into().unwrap()),
-    next_block: res.next_block.try_into().unwrap(),
-    total_execution_time: res.total_execution_time.try_into().unwrap(),
-    events,
-  })
+fn convert_response_to_query_response(res: skar_client::QueryResponse) -> Result<QueryResponse> {
+    let blocks = Block::from_batches(&res.data.blocks)
+        .context("map blocks from arrow")?
+        .into_values()
+        .collect();
+
+    let transactions = Transaction::from_batches(&res.data.transactions)
+        .context("map transactions from arrow")?
+        .into_values()
+        .collect();
+
+    let logs = Log::from_batches(&res.data.logs)
+        .context("map logs from arrow")?
+        .into_values()
+        .collect();
+
+    Ok(QueryResponse {
+        archive_height: res
+            .archive_height
+            .map(|h| h.try_into())
+            .transpose()
+            .context("convert height")?,
+        next_block: res.next_block.try_into().context("convert next_block")?,
+        total_execution_time: res
+            .total_execution_time
+            .try_into()
+            .context("convert total_execution_time")?,
+        data: QueryResponseData {
+            blocks,
+            transactions,
+            logs,
+        },
+    })
 }
--- src/query.rs
+++ src/query.rs
@@ -4,44 +4,44 @@ use serde::Serialize;
 #[napi(object)]
 #[derive(Default, Clone, Serialize)]
 pub struct LogSelection {
-  pub address: Vec<String>,
-  pub topics: Vec<Vec<String>>,
+    pub address: Vec<String>,
+    pub topics: Vec<Vec<String>>,
 }
 
 #[napi(object)]
 #[derive(Default, Clone, Serialize)]
 pub struct TransactionSelection {
-  pub from: Vec<String>,
-  pub to: Vec<String>,
-  pub sighash: Vec<String>,
-  pub status: Option<i64>,
+    pub from: Vec<String>,
+    pub to: Vec<String>,
+    pub sighash: Vec<String>,
+    pub status: Option<i64>,
 }
 
 #[napi(object)]
 #[derive(Default, Clone, Serialize)]
 pub struct FieldSelection {
-  pub block: Vec<String>,
-  pub transaction: Vec<String>,
-  pub log: Vec<String>,
+    pub block: Vec<String>,
+    pub transaction: Vec<String>,
+    pub log: Vec<String>,
 }
 
 #[napi(object)]
 #[derive(Default, Clone, Serialize)]
 pub struct Query {
-  pub from_block: i64,
-  pub to_block: Option<i64>,
-  pub logs: Vec<LogSelection>,
-  pub transactions: Vec<TransactionSelection>,
-  pub include_all_blocks: bool,
-  pub field_selection: FieldSelection,
-  pub max_num_blocks: Option<i64>,
-  pub max_num_transactions: Option<i64>,
-  pub max_num_logs: Option<i64>,
+    pub from_block: i64,
+    pub to_block: Option<i64>,
+    pub logs: Vec<LogSelection>,
+    pub transactions: Vec<TransactionSelection>,
+    pub include_all_blocks: bool,
+    pub field_selection: FieldSelection,
+    pub max_num_blocks: Option<i64>,
+    pub max_num_transactions: Option<i64>,
+    pub max_num_logs: Option<i64>,
 }
 
 impl Query {
-  pub fn try_convert(&self) -> Result<skar_net_types::Query> {
-    let json = serde_json::to_vec(self).context("serialize to json")?;
-    serde_json::from_slice(&json).context("parse json")
-  }
+    pub fn try_convert(&self) -> Result<skar_net_types::Query> {
+        let json = serde_json::to_vec(self).context("serialize to json")?;
+        serde_json::from_slice(&json).context("parse json")
+    }
 }
