912d1ce039207198bbc95072b410756d0ece86bf
Ozgur Akkurt <oezgurmakkurt@gmail.com>
2023-12-12 14:13:09 +0300

    finish erc20 example

--- examples/all-erc20/README.md
+++ examples/all-erc20/README.md
@@ -2,4 +2,4 @@
 
 Example of using hypersync-client to get data for all erc20 transfers.
 
-Can run `npx tsc && dist/app.js` to run it.
+Can run `npx tsc && node dist/app.js` to run it.
---
+++ examples/all-erc20/src/app.ts
@@ -0,0 +1,101 @@
+import { HypersyncClient, Decoder } from "@envio-dev/hypersync-client";
+import fs from "node:fs";
+import * as ethers from "ethers";
+
+async function main() {
+    // Create hypersync client using the mainnet hypersync endpoint
+    const client = await HypersyncClient.new({
+        url: "https://eth.hypersync.xyz"
+    });
+
+    // The query to run
+    const query = {
+        // start from block 0 and go to the end of the chain (we don't specify a toBlock).
+        "fromBlock": 0,
+        // The logs we want. We will also automatically get transactions and blocks relating to these logs (the query implicitly joins them).
+        "logs": [
+          {
+            // We want All ERC20 transfers so no address filter and only a filter for the first topic
+            "topics": [
+              ["0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef"],
+            ]
+          }
+        ],
+        // Select the fields we are interested in, notice topics are selected as topic0,1,2,3
+        "fieldSelection": {
+          "block": [
+            "number",
+            "timestamp",
+            "hash"
+          ],
+          "log": [
+            "block_number",
+            "log_index",
+            "transaction_index",
+            "transaction_hash",
+            "data",
+            "address",
+            "topic0",
+            "topic1",
+            "topic2",
+            "topic3"
+          ],
+          "transaction": [
+            "block_number",
+            "transaction_index",
+            "hash",
+            "from",
+            "to",
+            "value",
+            "input"
+          ]
+        },
+        max_num_logs: 10,
+      };
+
+    console.log("Running the query...");
+
+    // Run the query once, the query is automatically paginated so it will return when it reaches some limit (time, response size etc.)
+    //  there is a nextBlock field on the response object so we can set the fromBlock of our query to this value and continue our query until
+    // res.nextBlock is equal to res.archiveHeight or query.toBlock in case we specified an end block.
+    const res = await client.sendReq(query);
+
+    console.log(`Ran the query once. Next block to query is ${res.nextBlock}`);
+
+    // Filter zero value transfers, this makes decoding fail
+    const filteredLogs = res.data.logs.filter(log => log.data !== "x0");
+
+    // read json abi file for erc20
+    const abi = fs.readFileSync('./erc20.abi.json', 'utf8');
+    const parsedAbi = JSON.parse(abi);
+
+    // Map of contract_address -> ABI
+    let abis = {};
+
+    // every log we get should be decodable by this abi but we don't know
+    //  the specific contract addresses since we are indexing all erc20 transfers.
+    for (const log of res.data.logs) {
+      abis[log.address] = parsedAbi;
+    }
+
+    // Create a decoder with our mapping
+    const decoder = Decoder.new(abis);
+
+    // Decode the log on a background thread so we don't block the event loop.
+    // Can also use decoder.decodeLogsSync if it is more convenient.
+    const decodedLogs = await decoder.decodeLogs(res.data.logs);
+
+    // Let's count total volume, it is meaningless because of currency differences but good as an example.
+    let total_volume = BigInt(0);
+
+    for (const log of decodedLogs) {
+      // We know it is a bigint because of ABI
+      total_volume += log.body[0].val as bigint;
+    }
+
+    const totalBlocks = res.nextBlock - query.fromBlock;
+
+    console.log(`Total volume was ${total_volume} in ${totalBlocks} blocks`);
+}
+
+main();
