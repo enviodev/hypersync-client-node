1895a77d2de42b9833dc1af20009004735345690
Ozgur Akkurt <oezgurmakkurt@gmail.com>
2024-02-08 04:45:30 +0600

    add new parquet api

---
+++ examples/parquet-out/README.md
@@ -0,0 +1,5 @@
+# Parquet output example
+
+Example of using hypersync-client to get bulk data into parquet files.
+
+Can run `npx tsc && node dist/app.js` to run it.
---
+++ examples/parquet-out/src/app.ts
@@ -0,0 +1,59 @@
+import { HypersyncClient } from "@envio-dev/hypersync-client";
+import fs from "node:fs";
+
+async function main() {
+    // Create hypersync client using the mainnet hypersync endpoint
+    const client = HypersyncClient.new({
+      url: "https://eth.hypersync.xyz"
+    });
+
+    // The query to run
+    const query = {
+        "fromBlock": 18500123,
+        "toBlock": 19500123,
+        "transactions": [{}],
+        // Select the fields we are interested in, notice topics are selected as topic0,1,2,3
+        "fieldSelection": {
+          "block": [
+            "number",
+            "timestamp",
+            "hash"
+          ],
+          "log": [
+            "block_number",
+            "log_index",
+            "transaction_index",
+            "transaction_hash",
+            "data",
+            "address",
+            "topic0",
+            "topic1",
+            "topic2",
+            "topic3"
+          ],
+          "transaction": [
+            "block_number",
+            "transaction_index",
+            "hash",
+            "from",
+            "to",
+            "value",
+            "input"
+          ]
+        },
+      };
+
+    console.log("Writing to parquet... This might take some time depending on connection speed");
+
+    await client.createParquetFolder(query, {
+      path: "data",
+      /// retry internal requests forever so we don't fail on a bad connection
+      retry: true,
+      /// Convert binary columns to prefixed hex format like '0x1ab..'
+      hexOutput: true,
+    });
+
+    console.log("finished writing parquet");
+}
+
+main();
