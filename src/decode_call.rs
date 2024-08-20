65bceae7136b4a9ff26d99dcd7bdb7ab74b62aaa
Mikhail Sozin <mikhail.sozin@chainstack.com>
2024-08-20 20:05:59 +0200

    Additional methods

--- src/decode_call.rs
+++ src/decode_call.rs
@@ -1,5 +1,5 @@
 use crate::map_err;
-use crate::types::DecodedSolValue;
+use crate::types::{DecodedSolValue, Trace, Transaction};
 use anyhow::Context;
 use hypersync_client::format::{Data, Hex};
 use std::sync::Arc;
@@ -25,6 +25,21 @@ impl CallDecoder {
         })
     }
 
+    #[napi]
+    pub fn from_signatures_with_checksum(
+        signatures: Vec<String>,
+        checksum: bool,
+    ) -> napi::Result<CallDecoder> {
+        let inner = hypersync_client::CallDecoder::from_signatures(&signatures)
+            .context("build inner decoder")
+            .map_err(map_err)?;
+
+        Ok(Self {
+            inner: Arc::new(inner),
+            checksummed_addresses: checksum,
+        })
+    }
+
     pub fn enable_checksummed_addresses(&mut self) {
         self.checksummed_addresses = true;
     }
@@ -34,28 +49,83 @@ impl CallDecoder {
     }
 
     #[napi]
-    pub async fn decode_input(&self, input: String) -> Option<Vec<DecodedSolValue>> {
+    pub async fn decode_inputs(&self, inputs: Vec<String>) -> Vec<Option<Vec<DecodedSolValue>>> {
         let decoder = self.clone();
 
-        tokio::task::spawn_blocking(move || decoder.decode_input_sync(input))
+        tokio::task::spawn_blocking(move || decoder.decode_inputs_sync(inputs))
             .await
             .unwrap()
     }
 
     #[napi]
-    pub fn decode_input_sync(&self, input: String) -> Option<Vec<DecodedSolValue>> {
+
+    pub async fn decode_transactions_input(
+        &self,
+        txs: Vec<Transaction>,
+    ) -> Vec<Option<Vec<DecodedSolValue>>> {
+        let decoder = self.clone();
+
+        tokio::task::spawn_blocking(move || decoder.decode_transactions_input_sync(txs))
+            .await
+            .unwrap()
+    }
+
+    #[napi]
+    pub async fn decode_traces_input(
+        &self,
+        traces: Vec<Trace>,
+    ) -> Vec<Option<Vec<DecodedSolValue>>> {
+        let decoder = self.clone();
+
+        tokio::task::spawn_blocking(move || decoder.decode_traces_input_sync(traces))
+            .await
+            .unwrap()
+    }
+
+    #[napi]
+    pub fn decode_inputs_sync(&self, inputs: Vec<String>) -> Vec<Option<Vec<DecodedSolValue>>> {
+        inputs
+            .into_iter()
+            .map(|input| self.decode_impl(input))
+            .collect()
+    }
+
+    #[napi]
+    pub fn decode_transactions_input_sync(
+        &self,
+        txs: Vec<Transaction>,
+    ) -> Vec<Option<Vec<DecodedSolValue>>> {
+        txs.into_iter()
+            .map(|tx| self.decode_impl(tx.input?))
+            .collect()
+    }
+
+    #[napi]
+    pub fn decode_traces_input_sync(
+        &self,
+        traces: Vec<Trace>,
+    ) -> Vec<Option<Vec<DecodedSolValue>>> {
+        traces
+            .into_iter()
+            .map(|trace| self.decode_impl(trace.input?))
+            .collect()
+    }
+
+    #[napi]
+    pub fn decode_impl(&self, input: String) -> Option<Vec<DecodedSolValue>> {
         let input = Data::decode_hex(input.as_str())
             .context("decode input")
             .unwrap();
-
-        self.inner
+        let decoded_input = self
+            .inner
             .decode_input(&input)
             .context("decode log")
-            .unwrap()
-            .map(|v| {
-                v.into_iter()
-                    .map(|value| DecodedSolValue::new(value, self.checksummed_addresses))
-                    .collect()
-            })
+            .unwrap();
+        decoded_input.map(|decoded_input| {
+            decoded_input
+                .into_iter()
+                .map(|value| DecodedSolValue::new(value, self.checksummed_addresses))
+                .collect()
+        })
     }
 }
